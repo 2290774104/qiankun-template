7e5fe9dc20f5558891ea262f63b88bd1c9e8f5c6
2290774104 <2290774104@qq.com>
2024-06-18 16:50:48 +0800

    feat: 创建基座项目

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -5,19 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-import qiankun from 'vite-plugin-qiankun'
-const { name } = require('./package')
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools(), qiankun(name, { useDevMode: true })],
-  server: {
-    headers: {
-      'access-control-allow-origin': '*',
-      host: '0.0.0.0',
-      port: 8081
-    }
-  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
